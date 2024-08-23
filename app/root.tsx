ee9639b6d10d9d36480f45fe16b7280b0d5161af
Nahuel Luca <nahuelluca20@gmail.com>
2024-08-23 17:25:39 -0300

    chore: Add theme support and mode toggle button

--- app/root.tsx
+++ app/root.tsx
@@ -4,25 +4,47 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
 import stylesheet from "./tailwind.css?url";
-import { LinksFunction } from "@remix-run/cloudflare";
+import { LinksFunction, LoaderFunction } from "@remix-run/cloudflare";
+import {
+  ThemeProvider,
+  Theme,
+  PreventFlashOnWrongTheme,
+  useTheme,
+} from "remix-themes";
+import ModeToggle from "./components/buttons/mode-toggle";
+import { themeSessionResolver } from "./routes/theme-sessions.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export const loader: LoaderFunction = async ({ request }) => {
+  const { getTheme } = await themeSessionResolver(request);
+  const theme = getTheme();
+
+  return { theme };
+};
+
+function App() {
+  const data: {
+    theme: string;
+  } = useLoaderData();
+  const [theme] = useTheme();
   return (
-    <html lang="en">
+    <html lang="en" data-mode={theme ?? ""}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data?.theme)} />
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
+      <body className="px-5 max-w-[1200px] w-full mx-auto mt-10 bg-foreground">
+        <ModeToggle />
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -30,6 +52,16 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
-  return <Outlet />;
+export default function AppWithProviders() {
+  const data: {
+    theme: string;
+  } = useLoaderData();
+  return (
+    <ThemeProvider
+      specifiedTheme={data.theme as Theme}
+      themeAction="/action/set-theme"
+    >
+      <App />
+    </ThemeProvider>
+  );
 }
