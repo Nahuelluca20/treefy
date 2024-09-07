64b14bd56b22f1aac54c3d3859165cf05af22e60
Nahuel Luca <nahuelluca20@gmail.com>
2024-09-06 21:51:35 -0300

    add: remove sidebar for a while

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -1,6 +1,6 @@
 import { LoaderFunctionArgs } from "@remix-run/cloudflare";
-import { Outlet, useLoaderData } from "@remix-run/react";
-import SideBar from "~/components/navigation/side-bar";
+import { Outlet } from "@remix-run/react";
+// import SideBar from "~/components/navigation/side-bar";
 import { SessionStorage } from "~/modules/session.server";
 import { UserSession } from "~/types/user";
 
@@ -11,13 +11,13 @@ export async function loader({ context, request }: LoaderFunctionArgs) {
 }
 
 export default function Layout() {
-  const user = useLoaderData<typeof loader>();
+  // const user = useLoaderData<typeof loader>();
 
   return (
     <section className="px-5 max-w-[1200px] w-full mx-auto mt-10 bg-foreground">
       <section className="md:flex md:gap-20 lg:gap-36 items-start">
-        <SideBar userName={user.name ?? ""} />
-        <hr className="hidden lg:block" />
+        {/* <SideBar userName={user.name ?? ""} /> */}
+        {/* <hr className="hidden lg:block" /> */}
         <section className="max-w-[750px] xl:w-full mx-auto px-1 md:px-8 py-6">
           <Outlet />
         </section>
