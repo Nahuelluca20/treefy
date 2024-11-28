0fa2308e504680c679aaa7444487f4830e3cc328
Nahuel Luca <nahuelluca20@gmail.com>
2024-11-28 13:03:11 -0300

    Add CFImages plugin for image uploading

--- app/components/editor/plugins.ts
+++ app/components/editor/plugins.ts
@@ -6,6 +6,7 @@ import Link from "@yoopta/link";
 import { HeadingOne, HeadingTwo, HeadingThree } from "@yoopta/headings";
 import { NumberedList, BulletedList, TodoList } from "@yoopta/lists";
 import Image from "@yoopta/image";
+import { CFImages } from "cf-images";
 
 export const plugins = [
   Paragraph,
@@ -19,5 +20,30 @@ export const plugins = [
   TodoList,
   Link,
   Code,
-  Image,
+  Image.extend({
+    options: {
+      onUpload: async (file: File) => {
+        const cfImages = new CFImages({
+          token: "LFsRzjEcCNMHc9jueYuYxpSHIEm-WAtWg2-VUnby",
+          accountId: "e4ec2904f32944ac7ceb43c2740f3ca3",
+          imageAccountHash: "4wV7CQ2npJgGZmWNIBOGqA",
+        });
+        console.log(file);
+
+        const upload = await cfImages.uploadImage({
+          file: file,
+          requireSignedURLs: false,
+          metadata: { sarasa: "saras" },
+        });
+        console.log(upload);
+        return {
+          src: upload.result.variants[0],
+          sizes: {
+            width: 600,
+            height: 600,
+          },
+        };
+      },
+    },
+  }),
 ];
